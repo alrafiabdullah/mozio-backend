43984cac6b2d6bad8d62b05da544177602bf0f59
alrafiabdullah <abdullah34alrafi@gmail.com>
2021-08-17 17:50:33 +0600

    provider url added

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,8 @@
+from main.serializers import ProviderSerializer
 from django.urls import path
 
-urlpatterns = [
+from . import views
 
+urlpatterns = [
+    path('provider', views.ProviderClass.as_view(), name='provider'),
 ]
