9f88c26f7c5c20417220219470260b1f3d5842ef
alrafiabdullah <abdullah34alrafi@gmail.com>
2021-08-17 17:50:11 +0600

    Provider model updated and migrated

--- main/models.py
+++ main/models.py
@@ -1,17 +1,40 @@
+from operator import mod
 from django.db import models
-from django.db.models.fields import CharField
-from djmoney.models.fields import MoneyField
 from phonenumber_field.modelfields import PhoneNumberField
 
 
 # Create your models here.
 class Provider(models.Model):
+    LANGUAGE_CHOICES = (
+        ('bn', 'Bengali'),
+        ('en', 'English'),
+        ('es', 'Spanish'),
+        ('fr', 'French'),
+        ('de', 'German'),
+        ('it', 'Italian'),
+        ('pt', 'Portuguese'),
+        ('ru', 'Russian'),
+        ('ja', 'Japanese'),
+        ('zh', 'Chinese'),
+    )
+
+    CURRENCY_CHOICES = (
+        ('bdt', 'BDT'),
+        ('usd', 'USD'),
+        ('eur', 'EUR'),
+        ('gbp', 'GBP'),
+        ('cny', 'CNY'),
+        ('jpy', 'JPY'),
+        ('rub', 'RUB'),
+    )
+
     name = models.CharField(max_length=100, default="")
-    email = models.EmailField(default="")
+    email = models.EmailField(unique=True)
     phone_number = PhoneNumberField(default="")
-    language = CharField(default="", max_length=50)
-    currency = MoneyField(max_digits=1000, default=1.00,
-                          decimal_places=2, default_currency='USD')
+    language = models.CharField(
+        max_length=2, choices=LANGUAGE_CHOICES, default="bn")
+    currency = models.CharField(
+        max_length=3, choices=CURRENCY_CHOICES, default="bdt")
 
     def __str__(self):
         return self.name
