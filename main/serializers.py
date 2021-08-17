1d41ea84a5b134cd4d2debfc8f09b3909629cf74
alrafiabdullah <abdullah34alrafi@gmail.com>
2021-08-17 19:58:15 +0600

    Service area serializer added

--- main/serializers.py
+++ main/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework.serializers import ModelSerializer
 
-from .models import Provider
+from .models import Provider, ServiceArea
 
 
 class ProviderSerializer(ModelSerializer):
     class Meta:
         model = Provider
+        fields = '__all__'
+
+
+class ServiceAreaSerializer(ModelSerializer):
+    class Meta:
+        model = ServiceArea
         fields = '__all__'
