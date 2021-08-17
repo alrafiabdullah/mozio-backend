66f1c67eb5602378fc8f0807f4215fcda8fb93a7
alrafiabdullah <abdullah34alrafi@gmail.com>
2021-08-17 20:16:07 +0600

    lat lng check url added

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('provider', views.ProviderClass.as_view(), name='provider'),
     path('service_area', views.ServiceAreaClass.as_view(), name='service_area'),
+    path('check', views.CheckServiceArea.as_view(), name='check'),
 ]
