e41fc41cafecbe1512442c1b896a0b8aa23644cc
alrafiabdullah <abdullah34alrafi@gmail.com>
2021-08-17 17:11:31 +0600

    Provider model added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models.fields import CharField
+from djmoney.models.fields import MoneyField
+from phonenumber_field.modelfields import PhoneNumberField
+
 
 # Create your models here.
+class Provider(models.Model):
+    name = models.CharField(max_length=100, default="")
+    email = models.EmailField(default="")
+    phone_number = PhoneNumberField(default="")
+    language = CharField(default="", max_length=50)
+    currency = MoneyField(max_digits=1000, default=1.00,
+                          decimal_places=2, default_currency='USD')
+
+    def __str__(self):
+        return self.name
