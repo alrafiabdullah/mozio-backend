f6f0643e535af6827c4e742efd5505d101f97c2f
alrafiabdullah <abdullah34alrafi@gmail.com>
2021-08-17 19:58:06 +0600

    Service area model added

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from leaflet.admin import LeafletGeoAdmin
 
-from .models import Provider
+from .models import Provider, ServiceArea
 
 
 # Register your models here.
 admin.site.register(Provider)
+admin.site.register(ServiceArea, LeafletGeoAdmin)
--- main/models.py
+++ main/models.py
@@ -1,5 +1,4 @@
-from operator import mod
-from django.db import models
+from django.contrib.gis.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
 
@@ -28,13 +27,23 @@ class Provider(models.Model):
         ('rub', 'RUB'),
     )
 
-    name = models.CharField(max_length=100, default="")
-    email = models.EmailField(default="")
-    phone_number = PhoneNumberField(default="")
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = PhoneNumberField()
     language = models.CharField(
-        max_length=2, choices=LANGUAGE_CHOICES, default="bn")
+        max_length=2, choices=LANGUAGE_CHOICES)
     currency = models.CharField(
-        max_length=3, choices=CURRENCY_CHOICES, default="bdt")
+        max_length=3, choices=CURRENCY_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+
+class ServiceArea(models.Model):
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, default="")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.PolygonField()
 
     def __str__(self):
         return self.name
